efaf81b22a9e5331d4a87e34692f2bee9ff94187
F.M. Rahat Mahamud Refat <rahat01939304@gmail.com>
2025-08-10 03:48:07 +0600

    Update config.py

--- config.py
+++ config.py
@@ -1,9 +1,9 @@
 db_config = {
-    'host': 'localhost',
-    'port': 3307,
-    'user': 'root',
-    'password': 'Rahat007700',
-    'database': 'pharmacy'
+    'host': 'gateway01.ap-southeast-1.prod.aws.tidbcloud.com',
+    'port': 4000,
+    'user': 'Z1cofXdfkRhWbo4.root',
+    'password': 'qnNJtIqf3Yk3U7FI',
+    'database': 'test'
 }
 
 SECRET_KEY = 'a_very_secret_and_long_random_string_for_flask_sessions'
